6a5fe3d3f34234bc9337e2677a597e8cf0fa64dd
Achilles-10 <1029991674@qq.com>
2017-11-18 16:28:16 +0800

    refresh

--- ALGORITHMS/selection_sort.py
+++ ALGORITHMS/selection_sort.py
@@ -1,19 +1,18 @@
 def selection_sort_ascend(list):
 	for j in range(0, len(list)):
-		i = j + 1
 		min = j
-		while i < len(list):
+		for i in range(j + 1, len(list)):
 			if list[i] < list[min]:
 				min = i
 			i += 1
 		list[min], list[j] = list[j], list[min]
 	return list
 
+
 def selection_sort_descend(list):
 	for j in range(0, len(list)):
-		i = j + 1
 		max = j
-		while i < len(list):
+		for i in range(j + 1, len(list)):
 			if list[i] > list[max]:
 				max = i
 			i += 1
