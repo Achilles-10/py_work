f6e63b779aedbcc0e7c7218c3356c3838463d34e
Achilles-10 <1029991674@qq.com>
2017-11-27 23:17:25 +0800

    weather

---
+++ data_visual/16_1/highs_low_2014.py
@@ -0,0 +1,28 @@
+import csv
+from datetime import datetime
+from matplotlib import pyplot as plt
+
+filename = 'sitka_weather_2014.csv'
+with open(filename) as f:
+	reader = csv.reader(f)
+	header_row = next(reader)
+
+	dates, highs = [], []
+	for row in reader:
+		current_date = datetime.strptime(row[0], '%Y-%m-%d')
+		dates.append(current_date)
+		high = int(row[1])
+		highs.append(high)
+
+	print(highs)
+
+fig = plt.figure(dpi=128, figsize=(16, 9))
+plt.plot(dates, highs, c='red')
+
+plt.title("Daily high temperatures - 2014", fontsize=24)
+plt.xlabel('', fontsize=16)
+fig.autofmt_xdate()
+plt.ylabel('Temperatures (F)', fontsize=16)
+plt.tick_params(axis='both', which='major', labelsize=16)
+
+plt.show()
