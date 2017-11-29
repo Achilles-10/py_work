82dfbd1ef08260abd9ac2bf74ea0d76a74ebf74d
Achilles-10 <1029991674@qq.com>
2017-11-29 19:50:59 +0800

    world_population

---
+++ data_visual/16_2/americas.py
@@ -0,0 +1,12 @@
+from pygal.maps.world import World
+
+wm = World()
+wm.force_uri_protocol = 'http'
+wm.title = 'North, Central, an South America'
+
+wm.add('North America', ['ca', 'mx', 'us'])
+wm.add('Central America', ['bz', 'cr', 'gt', 'hn', 'pa', 'sv'])
+wm.add('South America',
+       ['ar', 'bo', 'cl', 'co', 'ec', 'gf', 'gy', 'pe', 'py', 'sr', 'uy', 've'])
+
+wm.render_to_file('america.svg')
